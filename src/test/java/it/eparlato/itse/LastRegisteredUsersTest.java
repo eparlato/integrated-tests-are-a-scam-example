f6ba39c7a772715a69b26c408a7b467ab56b5d03
Edoardo Parlato <edoardo.parlato@xpeppers.com>
2019-01-19 19:44:22 +0100

    Empty project with working junit

---
+++ src/test/java/it/eparlato/itse/LastRegisteredUsersTest.java
@@ -0,0 +1,13 @@
+package it.eparlato.itse;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class LastRegisteredUsersTest {
+    @Test
+    public void junitWorks() {
+        assertThat(true, is(true));
+    }
+}
