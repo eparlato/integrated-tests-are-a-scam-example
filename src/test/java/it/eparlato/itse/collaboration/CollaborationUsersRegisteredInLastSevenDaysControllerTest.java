6e6283b7ca297a0292dd0fb6337d5352dd5705af
Edoardo Parlato <edoardo.parlato@xpeppers.com>
2019-01-24 19:12:56 +0100

    Refactoring

--- src/test/java/it/eparlato/itse/collaboration/CollaborationUsersRegisteredInLastSevenDaysControllerTest.java
+++ src/test/java/it/eparlato/itse/collaboration/CollaborationUsersRegisteredInLastSevenDaysControllerTest.java
@@ -9,7 +9,6 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -46,7 +45,7 @@ public class CollaborationUsersRegisteredInLastSevenDaysControllerTest {
 
     @Test
     public void shouldShowCustomerDetailPageIfThereIsOnlyOneCustomerRegisteredSevenDaysBefore() throws Exception {
-        final List<Customer> expectedCustomersList = Arrays.asList(new Customer(DateUtils.calendarFromString("10/01/2019")));
+        final List<Customer> expectedCustomersList = Arrays.asList(newCustomer("10/01/2019"));
 
         context.checking(new Expectations(){{
             oneOf(usersRepository).findAllCustomersWhoHaveSignedUpSince(DateUtils.calendarFromString("10/01/2019"));
@@ -61,11 +60,11 @@ public class CollaborationUsersRegisteredInLastSevenDaysControllerTest {
     @Test
     public void shouldShowCustomersListIfThereAreFewCustomersRegisteredSevenDaysBefore() throws Exception {
         final List<Customer> expectedCustomersList = new ArrayList<Customer>();
-        expectedCustomersList.add(new Customer(DateUtils.calendarFromString("08/01/2019")));
-        expectedCustomersList.add(new Customer(DateUtils.calendarFromString("09/01/2019")));
-        expectedCustomersList.add(new Customer(DateUtils.calendarFromString("10/01/2019")));
-        expectedCustomersList.add(new Customer(DateUtils.calendarFromString("11/01/2019")));
-        expectedCustomersList.add(new Customer(DateUtils.calendarFromString("12/01/2019")));
+        expectedCustomersList.add(newCustomer("08/01/2019"));
+        expectedCustomersList.add(newCustomer("09/01/2019"));
+        expectedCustomersList.add(newCustomer("10/01/2019"));
+        expectedCustomersList.add(newCustomer("11/01/2019"));
+        expectedCustomersList.add(newCustomer("12/01/2019"));
 
 
         context.checking(new Expectations(){{
@@ -77,5 +76,11 @@ public class CollaborationUsersRegisteredInLastSevenDaysControllerTest {
         controller.showRegisteredUserSevenDaysBackFrom(DateUtils.calendarFromString("20/01/2019"));
 
         assertThat(webPage.showCustomersListHasBeenCalled(), is(true));
+    }
+
+    // TODO: continue with other collaboration tests
+
+    private Customer newCustomer(String registrationDate) throws Exception {
+        return new Customer (DateUtils.calendarFromString(registrationDate));
     }
 }
